80e53ecc09c70a51cc761c85c95c9910a173fc4c
julyano <cardoso.juliano@gmail.com>
2018-10-18 13:31:53 -0300

    corrigindo MainController

--- src/main/java/br/com/esig/web/MainController.java
+++ src/main/java/br/com/esig/web/MainController.java
@@ -3,11 +3,13 @@ package br.com.esig.web;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class MainController {
 	
 	@RequestMapping(value="/", method=RequestMethod.GET)
+	@ResponseBody
 	public String index() { 
 		return "index"; 
 	}
